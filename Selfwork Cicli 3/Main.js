e3ca271e179bbbedea9bd7d197a6eab95137ba60
FaustNFurious <lorelego2003@gmail.com>
2024-12-16 22:31:42 +0100

    Add files via upload
    corretto esrcizio

--- Selfwork Cicli 3/Main.js
+++ Selfwork Cicli 3/Main.js
@@ -1,23 +1,29 @@
 // Dichiarazione Variabili
-let num = parseInt(prompt("Inserire un numero"));
+let num;
 
 
 // Istruzioni per distribuire le bevande
-switch (true) {
+do {
+    num = parseInt(prompt("Inserire un numero per la bevanda -- 1: acqua, 2: coca cola, 3: birra"));
 
-    case num == 1:
-        console.log("E' stata selezionata l'acqua");
-        break;
+    switch (true) {
 
-    case num == 2:
-        console.log("E' stata selezionata coca cola");
-        break;
+        case num == 1:
+            console.log("E' stata selezionata l'acqua");
+            break;
 
-    case num == 3:
-        console.log("E' stata selezionata birra");
-        break;
+        case num == 2:
+            console.log("E' stata selezionata coca cola");
+            break;
 
-    default:
-        num = parseInt(prompt("Numero non valido\nInserire un numero"));
+        case num == 3:
+            console.log("E' stata selezionata birra");
+            break;
+
+        default:
+            console.log("Numero non valido");
         
-}
+    }
+
+} while (num < 1 || num > 3);
+    
